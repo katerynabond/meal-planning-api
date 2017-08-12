e655dc03d071a69e7ddf5e27c3c61d5756c9151d
Kateryna Bondareva <kbondareva@gmail.com>
2017-08-12 17:06:39 -0400

    more stubs

--- routers/meals.router.js
+++ routers/meals.router.js
@@ -2,9 +2,23 @@ const express = require ('express');
 const router = express.Router();
 // test data
 const meal1 = require('./testdata/meal1'); 
+const mealsList = require('./testdata/meals'); 
 
-router.get('/meal', (request, response) => {
-  response.json(meal1);
+
+router.get('/meal/:date/:course', (request, response) => {
+  console.log(`requesting date:${request.params.date} course:${request.params.course}`);
+  var d = new Date(request.params.date);
+  var mealToReturn = JSON.parse(JSON.stringify(meal1));
+  mealToReturn.description += ` (${request.params.course})`;
+  mealToReturn.description += ` for date:${d.toDateString()}`;
+  response.json(mealToReturn);
+});
+
+router.get('/meals/:course', (request, response) => {
+  console.log(`/meals/:course course:${request.params.course}`);
+  var list = JSON.parse(JSON.stringify(mealsList));
+  list[0].title += ` (${request.params.course})`;
+  response.json(list);
 });
 
 
---
+++ routers/testdata/meals.js
@@ -0,0 +1,28 @@
+const meals = [
+    {
+        title: 'Kasha',
+        description: 'Energizing and nutritious, buckwheat is available throughout the year and can be served as an alternative to rice or made into porridge.',
+        calories: '100 Cal',
+        cookingTime: '15 min',
+        added: 0,
+        likes: 23
+    },
+    {
+        title: 'Kasha 2',
+        description: 'Energizing and nutritious, buckwheat is available throughout the year and can be served as an alternative to rice or made into porridge.',
+        calories: '100 Cal',
+        cookingTime: '15 min',
+        added: 1,
+        likes: 23
+    },
+    {
+        title: 'Kasha 3',
+        description: 'Energizing and nutritious, buckwheat is available throughout the year and can be served as an alternative to rice or made into porridge.',
+        calories: '100 Cal',
+        cookingTime: '15 min',
+        added: 0,
+        likes: 66
+    },
+];
+
+module.exports = meals;
